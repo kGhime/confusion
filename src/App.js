4fb4ba7abc17b98cede9e27a75b1efdcec471eeb
kGhime <kghime@esri.com>
2020-07-22 14:37:54 -0700

    components part 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes";
 
 //function based component
 // function App() {
@@ -28,6 +29,13 @@ import "./App.css";
 
 //class based component
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
       <div>
@@ -36,7 +44,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
