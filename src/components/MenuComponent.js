7257024fb8d6245243331a0e801de646ab1f3c91
kGhime <kghime@esri.com>
2020-07-30 17:02:26 -0700

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,19 +7,10 @@ import {
   CardBody,
   CardTitle,
 } from "reactstrap";
-import Dishdetail from "./DishdetailComponent";
 
 class Menu extends Component {
   constructor(props) {
     super(props); //required when defining class component
-
-    this.state = {
-      selectedDish: null,
-    };
-  }
-
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
   }
 
   componentDidMount() {
@@ -46,7 +37,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onDishSelect(dish)}>
+          <Card onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" object src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -61,7 +52,6 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <Dishdetail dish={this.state.selectedDish} />
       </div>
     );
   }
