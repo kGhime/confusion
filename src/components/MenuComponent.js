c6b07112711635eb4244b28554b5ad88ea38d985
kGhime <kghime@esri.com>
2020-08-16 18:34:36 -0700

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,6 +8,7 @@ import {
   BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 // simply rendering menu items based on the dish informationv sent and based on onclick, view is rendered in reactrap card
 function RenderMenuItem({ dish, onClick }) {
@@ -26,7 +27,7 @@ function RenderMenuItem({ dish, onClick }) {
 
 //same implementation of functioon like above but in ES6 way using arrow function syntax
 const Menu = (props) => {
-  const menu = props.dishes.map((dish) => {
+  const menu = props.dishes.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
         <RenderMenuItem dish={dish} />
@@ -34,23 +35,40 @@ const Menu = (props) => {
     );
   });
 
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/home">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Menu</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>Menu</h3>
-          <hr />
+  if (props.dishes.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
         </div>
       </div>
-      <div className="row">{menu}</div>
-    </div>
-  );
+    );
+  } else if (props.dishes.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.dishes.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Menu</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">{menu}</div>
+      </div>
+    );
 };
 
 export default Menu;
