50a54668028c5a616f39f6642d259851c44ba5df
kGhime <kghime@esri.com>
2020-08-20 01:19:46 -0700

    Assignment 4 -- Task 3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -10,20 +10,23 @@ import {
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderLeader({ leader }) {
   return (
     <div key={leader.id} className="col-12 md-5">
-      <Media tag="li">
-        <Media left middle>
-          <Media object src={baseUrl + leader.image} alt={leader.name} />
+      <Fade in>
+        <Media tag="li">
+          <Media left middle>
+            <Media object src={baseUrl + leader.image} alt={leader.name} />
+          </Media>
+          <Media body className="ml-5">
+            <Media heading>{leader.name}</Media>
+            <p>{leader.designation}</p>
+            <p>{leader.description}</p>
+          </Media>
         </Media>
-        <Media body className="ml-5">
-          <Media heading>{leader.name}</Media>
-          <p>{leader.designation}</p>
-          <p>{leader.description}</p>
-        </Media>
-      </Media>
+      </Fade>
     </div>
   );
 }
@@ -127,7 +130,9 @@ function About(props) {
             <h2>Corporate Leadership</h2>
           </div>
           <div className="col-12">
-            <Media list>{leaders}</Media>
+            <Media list>
+              <Stagger in>{leaders}</Stagger>
+            </Media>
             {/* <RenderLeader leaders={props.leaders} /> */}
           </div>
         </div>
